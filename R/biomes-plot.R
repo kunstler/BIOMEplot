09602cd778707508d8131e5bd1a025e2a739e66d
kunstler <georges.kunstler@gmail.com>
2015-05-07 10:33:31 +0200

    add variables description

--- R/biomes-plot.R
+++ R/biomes-plot.R
@@ -4,12 +4,9 @@
 ##' of mean annual temperature (MAT) and mean annual precipitation (MAP)
 ##' (MAT in degree Celsius and MAP in cm).
 ##' 
-##' @references R.H. Whittaker. 1975. Communities and Ecosystems.
-##' 2d ed. Macmillan New York.
+##' @references R.H. Whittaker. 1975. Communities and Ecosystems. 2d ed. Macmillan New York.
 ##' 
-##' @references Ricklefs, R. E. (2008). The economy of nature.
-##' W. H. Freeman and Company.
-##' Chapter 5, Biological Communities, The biome concept.
+##' @references Ricklefs, R. E. (2008). The economy of nature. W. H. Freeman and Company. Chapter 5, Biological Communities, The biome concept.
 ##'
 ##' @title fun.poly.obj
 ##' @return a SpatialPolygonsDataFrame of the biomes
@@ -48,6 +45,10 @@ fun.poly.obj <- function(){
 ##' @param poly biomes SpatialPolygons object 
 ##' @param col.biomes  vector of colors for the biomes (TODO add name of biomes)
 ##' @param add.legend  TRUE or FALSE
+##' @param add.number  TRUE or FALSE
+##' @param merge.deserts  TRUE or FALSE
+##' @param mar.vec vector of margine size
+##' @param cex.p size of pointed passed to par
 ##' @return plot
 ##' @author Kunstler
 ##' @export
@@ -63,8 +64,9 @@ plot_biome <- function(poly = fun.poly.obj()$poly.DF,
                                                            "olivedrab",
                                                            "gray"),
                                                          0.9),
-                       add.legend = TRUE,
-                       mar.vec = c(5.1, 3.5, 4.1, 2.1),
+                       add.legend = FALSE, add.number = TRUE,
+                       merge.deserts = FALSE,
+                       mar.vec = c(5.1, 3.5, 3, 2.1),
                        cex.p = 1.2){
   par(mar = mar.vec,
       mgp = c(1.8, 0.6,0),
@@ -73,7 +75,8 @@ plot_biome <- function(poly = fun.poly.obj()$poly.DF,
        xlab = "Mean annual precipitation (cm)",
        ylab = expression(paste('Mean annual temperature ', (~degree~C))))
   # MAKE POLYGONS
-  sp::plot(poly,col =  col.biomes,
+  if(!merge.deserts){
+  sp::plot(poly, col =  col.biomes,
            border = NA,
            add = TRUE)
    if(add.legend) {
@@ -84,6 +87,45 @@ plot_biome <- function(poly = fun.poly.obj()$poly.DF,
                                       "darkgreen","olivedrab","gray"),
                                       0.9),
              bty = 'n')}
+  if(add.number) {
+    x.num <- c(14.60469,  35.22922,  41.50625,
+           61.23406, 133.86827, 300.65795,
+           13.70797, 106.07000, 419.92153)
+
+    y.num <- c(-13.629438,  -5.127425,  14.977337,
+           16.577716,  17.177859,  18.178024,
+           27.680346,  27.180228,  24.479588)
+    text(x.num, y.num, labels = 1:9)
+  }
+ }else{
+  sp::plot(poly, col =  make.transparent(c("navajowhite3", "navajowhite3",
+                                      "sienna","darkolivegreen4",
+                                      "darkseagreen3", "forestgreen",
+                                      "darkgreen","olivedrab","gray"),
+                                      0.9),
+           border = NA,
+           add = TRUE)
+   if(add.legend) {
+      legend("topright", legend = poly$biomes[-2],
+             fill =make.transparent(c("navajowhite3",
+                                      "sienna","darkolivegreen4",
+                                      "darkseagreen3", "forestgreen",
+                                      "darkgreen","olivedrab","gray"),
+                                      0.9),
+             bty = 'n')}
+  if(add.number) {
+    x.num <- c(14.60469,  35.22922, 
+           61.23406, 133.86827, 300.65795,
+           13.70797, 106.07000, 419.92153)
+
+    y.num <- c(-13.629438,  -5.127425,
+           16.577716,  17.177859,  18.178024,
+           27.680346,  27.180228,  24.479588)
+    text(x.num, y.num, labels = 1:8)
+  }
+
+
+ }    
 }
 
 ##' make rgb color transparent
@@ -98,3 +140,5 @@ make.transparent <- function(col, opacity=0.5) {
   tmp <- col2rgb(col)/255
   rgb(tmp[1,], tmp[2,], tmp[3,], alpha=opacity)
 }
+
+
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ BIOMEplot
 
 ## Georges Kunstler <georges.kunstler@gmail.com>
 
-R package to plot Whittaker' biomes (Whittaker 1975).
+R package to plot Whittaker' biomes (Whittaker 1975). This is not possible to use this plot with ggplot2 (see Hadley (comment)[https://github.com/hadley/ggplot2/wiki/Mixing-ggplot2-graphs-with-other-graphical-output])
 
 The biomes MAP and MAT were digitalized from the modified biomes of Ricklefs (2008).
 
